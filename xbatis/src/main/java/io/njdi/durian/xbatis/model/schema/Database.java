240c1648ef884843b592fdfa279d379c6b4e242e
yurun <>
2022-02-28 19:39:06 +0800

    模块 xbatis 内置 Database 实例
    添加数据表时，数据表类可继承 Table，使用 @Component 标识，构造函数注入 Database 实例，然后声明数据表各属性信息，最后把自身添加到 Database 实例。

--- xbatis/src/main/java/io/njdi/durian/xbatis/model/schema/Database.java
+++ xbatis/src/main/java/io/njdi/durian/xbatis/model/schema/Database.java
@@ -1,36 +1,24 @@
 package io.njdi.durian.xbatis.model.schema;
 
-import lombok.*;
+import lombok.Data;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Component
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Database implements Checker {
-  @Singular
-  private List<Table> tables;
+  private final List<Table> tables = new ArrayList<>();
 
-  @Override
-  public void check() {
-    if (CollectionUtils.isEmpty(tables)) {
-      throw new RuntimeException("Database tables is empty(null)");
-    }
-
-    for (Table table : tables) {
-      if (Objects.isNull(table)) {
-        throw new RuntimeException("Database contains empty(null) table");
-      }
-
-      table.check();
-    }
+  public void addTable(Table table) {
+    tables.add(table);
   }
 
   public boolean containTable(String tableName) {
@@ -64,5 +52,20 @@ public class Database implements Checker {
                     StringUtils.isNotEmpty(table.getName())
                             && StringUtils.isNotEmpty(table.getAlias()))
             .collect(Collectors.toMap(Table::getName, Table::getAlias));
+  }
+
+  @Override
+  public void check() {
+    if (CollectionUtils.isEmpty(tables)) {
+      return;
+    }
+
+    for (Table table : tables) {
+      if (Objects.isNull(table)) {
+        throw new RuntimeException("Database contains empty(null) table");
+      }
+
+      table.check();
+    }
   }
 }
